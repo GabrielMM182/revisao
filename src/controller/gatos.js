63a00bb143a19a087bdf9a79635fa07ecf362f3a
GabrielMM182 <gmm_morais@hotmail.com>
2022-06-28 23:04:12 -0300

    adicionando busca por id e random

--- src/controller/gatos.js
+++ src/controller/gatos.js
@@ -1,9 +1,30 @@
-const { lista, salvarGatos } = require("../../dados");
+const { lista ,salvarGatos } = require("../../dados");
+
+const mesAleatorio = (req, res) => {
+    const randomMonth = lista[Math.floor(Math.random() * lista.length)];
+    res.json({ month: randomMonth })
+}
 
 const consultarTodosOsAnimais = (req, res) => {
-    res.json(lista);
-}
+  res.json(lista);
+};
+
+const consultarAnimalEspecifico = (req, res) => {
+    const animais = lista.find(
+        (animais) => animais.id === Number(req.params.idConsulta)
+    );
+
+    if (animais === undefined) {
+        res.status(404);
+        res.json({erro: "esse animal passado ->" + req.params.idConsulta + "não existe"});
+        return
+    }
+
+    res.json(animais);
+};
 
 module.exports = {
-    consultarTodosOsAnimais
-}
+  consultarTodosOsAnimais,
+  consultarAnimalEspecifico,
+  mesAleatorio
+};
